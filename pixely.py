5e331a0f5dfc679f25616b973c6a06bb5d5a109d
sadry_hll <sadry.hilal@epitech.digital>
2022-01-11 16:59:13 +0100

    [CREATE pixely.py]
    creation of the script, of the read.md and import of the images in the IMG folder.

---
+++ pixely.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+def pixelate(input_file_path, pixel_size):
+    image = Image.open(input_file_path)
+    image = image.resize(
+        (image.size[0] // pixel_size, image.size[1] // pixel_size),
+        Image.NEAREST
+    )
+    image = image.resize(
+        (image.size[0] * pixel_size, image.size[1] * pixel_size),
+        Image.NEAREST
+    )
+
+    image.show()
+
+pixelate("./IMG/aladdin.jpeg",3)
